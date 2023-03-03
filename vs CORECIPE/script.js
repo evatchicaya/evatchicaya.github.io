39f506ce3a4c6cc06c16bf6670d9b6dc15528b85
eva tchicaya <75638773+evatchicaya@users.noreply.github.com>
2023-03-02 23:57:17 -0600

    Add files via upload

---
+++ vs CORECIPE/script.js
@@ -0,0 +1,36 @@
+//africa
+var africaPin = $(".africa");
+var africaInfo = $(".africaInfo");
+africaPin.on("mouseover", displayInfo1);
+africaPin.on("mouseout", displayInfo1);
+
+//asia
+var asiaPin = $(".asia");
+var asiaInfo = $(".asiaInfo");
+madagascarPin.on("mouseover", displayInfo3);
+madagascarPin.on("mouseout", displayInfo3);
+
+//australia-oceania
+var australiaoceaniaPin = $(".australia-oceania");
+var australiaoceaniaInfo = $(".australia-oceaniaInfo");
+australia-oceaniaPin.on("mouseover", displayInfo4);
+australia-oceaniaPin.on("mouseout", displayInfo4);
+
+//europe
+var europePin = $(".europe");
+var europeInfo = $(".europeInfo");
+europePin.on("mouseover", displayInfo4);
+europePin.on("mouseout", displayInfo4);
+
+//north america
+var namericaPin = $(".namerica");
+var namericaInfo = $(".namericaInfo");
+namericaPin.on("mouseover", displayInfo4);
+namericaPin.on("mouseout", displayInfo4);
+
+//south america
+var samericaPin = $(".samerica");
+var samericaInfo = $(".samericaInfo");
+var samericaText = $(".samericaText");
+samericaPin.on("mouseover", displayInfo4);
+samericaPin.on("mouseout", displayInfo4);
